7fcd9b73bc3efdec5beeb8acebf418ab83999b42
Thein-maung <59556712+Thein-maung@users.noreply.github.com>
2025-11-16 04:07:00 +0630

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,19 @@ export default defineConfig({
   build: {
     target: 'es2020',
     rollupOptions: {
-      external: ['https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.21.0/dist/tf.min.js']
+      external: [
+        'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.21.0/dist/tf.min.js',
+        './twinnet.js'
+      ]
     }
   },
   optimizeDeps: {
     exclude: ['@tensorflow/tfjs']
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
   }
 });
